3217cc1cdfbdf8f5c5ef085e3c0123e49395748b
Oreinz Kasilag <2162789@gmail.com>
2019-06-17 22:37:24 +0800

    LoanMontitoringSystem

---
+++ LoanMonitoringSystem/ClientLoanAction.php
@@ -0,0 +1,117 @@
+<?php
+include "Include/connection.php";
+$number = count($_POST["due"]);
+$due = mysqli_real_escape_string($conn,$_POST['due']['0']);
+$client_id = mysqli_real_escape_string($conn,$_POST['client_id']);
+$amount = mysqli_real_escape_string($conn,$_POST['amount']);
+$interest = mysqli_real_escape_string($conn,$_POST['interest']);
+$mat_date = mysqli_real_escape_string($conn, $_POST["due"][$number-1]);
+$loan_class = mysqli_real_escape_string($conn,$_POST['loan_class']);
+$shf = mysqli_real_escape_string($conn,$_POST['shf']);
+$insurance = mysqli_real_escape_string($conn,$_POST['insurance']);
+$loan_type = mysqli_real_escape_string($conn,$_POST['loan_type']);
+
+    $co_first_name_one = mysqli_real_escape_string($conn, $_POST['co_first_name_one']);
+    $co_middle_name_one = mysqli_real_escape_string($conn, $_POST['co_middle_name_one']);
+    $co_last_name_one = mysqli_real_escape_string($conn, $_POST['co_last_name_one']);
+    $co_contact_no_one = mysqli_real_escape_string($conn, $_POST['co_contact_no_one']);
+    $co_address_one = mysqli_real_escape_string($conn, $_POST['co_address_one']);
+    $co_business_address_one = mysqli_real_escape_string($conn, $_POST['co_business_address_one']);
+    $co_name_of_firm_one = mysqli_real_escape_string($conn, $_POST['co_name_of_firm_one']);
+    $co_employment_one = mysqli_real_escape_string($conn, $_POST['co_employment_one']);
+    $co_position_one = mysqli_real_escape_string($conn, $_POST['co_position_one']);
+
+    //Co Borrower _two Table    
+    $co_first_name_two = mysqli_real_escape_string($conn, $_POST['co_first_name_two']);
+    $co_middle_name_two = mysqli_real_escape_string($conn, $_POST['co_middle_name_two']);
+    $co_last_name_two = mysqli_real_escape_string($conn, $_POST['co_last_name_two']);
+    $co_contact_no_two = mysqli_real_escape_string($conn, $_POST['co_contact_no_two']);
+    $co_address_two = mysqli_real_escape_string($conn, $_POST['co_address_two']);
+    $co_business_address_two = mysqli_real_escape_string($conn, $_POST['co_business_address_two']);
+    $co_name_of_firm_two = mysqli_real_escape_string($conn, $_POST['co_name_of_firm_two']);
+    $co_employment_two = mysqli_real_escape_string($conn, $_POST['co_employment_two']);
+    $co_position_two = mysqli_real_escape_string($conn, $_POST['co_position_two']);
+
+    
+        $sqlForCoBow2 = "SELECT co_borrower_id from co_borrower WHERE co_first_name = '$co_first_name_two' && co_middle_name='$co_middle_name_two' && co_last_name='$co_last_name_two'";
+        $CoBow2 = mysqli_fetch_assoc(mysqli_query($conn,$sqlForCoBow2));
+        $co_borrower_id2 = $CoBow2['co_borrower_id'];
+
+         $sqlForCoBow = "SELECT co_borrower_id from co_borrower WHERE co_first_name = '$co_first_name_one' && co_middle_name='$co_middle_name_one' && co_last_name='$co_last_name_one'";
+        $CoBow = mysqli_fetch_assoc(mysqli_query($conn,$sqlForCoBow));
+        $co_borrower_id = $CoBow['co_borrower_id'];
+
+    
+        $sqlForAmountCoBow = "SELECT coalesce(SUM(loan_balance),0) as lb from co_borrower join co_loan on co_loan.co_borrower_id = co_borrower.co_borrower_id join loan on loan.loan_id = co_loan.loan_id WHERE co_borrower.co_borrower_id ='$co_borrower_id'";
+        $AmountCoBow = mysqli_fetch_assoc(mysqli_query($conn,$sqlForAmountCoBow));
+        $Amount = $AmountCoBow['lb'];
+
+        $sqlForAmountCoBow2 = "SELECT coalesce(SUM(loan_balance),0) as lb from co_borrower join co_loan on co_loan.co_borrower_id = co_borrower.co_borrower_id join loan on loan.loan_id = co_loan.loan_id WHERE co_borrower.co_borrower_id ='$co_borrower_id2'";
+        $AmountCoBow2 = mysqli_fetch_assoc(mysqli_query($conn,$sqlForAmountCoBow2));
+        $Amount2 = $AmountCoBow['lb'];
+        $array = array();
+
+        for ($x = 0; $x < $number; $x++) {
+            $array[$x] = $_POST['due'][$x];
+        }
+        
+         ?>
+    <html>
+<head>
+<script type="text/javascript">
+<!--
+ window.onload = function() {
+  confirmation();
+};
+function confirmation() {
+	var answer = confirm("<?php echo $co_first_name_one,'- co borrowed amount is ',$Amount,'\n',$co_first_name_two,'- co borrowed amount is ',$Amount2,'\nIs this okay?'?>");
+    if (answer){
+		document.forLoan.submit();
+    }else{
+        document.getElementById('resend').click();
+	}
+}
+//-->
+</script>
+</head>
+<body>
+<form name='forLoan' action='ClientLoanActionAction.php' method='POST'>
+    <input type="hidden" value="<?php echo $number?>" name="number">
+    <?php 
+        foreach($array as $value)
+        {
+          echo '<input type="hidden" name="result[]" value="'. $value. '">';
+        }
+    ?>
+    <input type="hidden" value="<?php echo $client_id?>" name="client_id">
+    <input type="hidden" value="<?php echo $amount?>" name="amount">
+    <input type="hidden" value="<?php echo $interest?>" name="interest">
+    <input type="hidden" value="<?php echo $mat_date?>" name="mat_date">
+    <input type="hidden" value="<?php echo $loan_class?>" name="loan_class">
+    <input type="hidden" value="<?php echo $shf?>" name="shf">
+    <input type="hidden" value="<?php echo $insurance?>" name="insurance">
+    <input type="hidden" value="<?php echo $loan_type?>" name="loan_type">
+    
+    <input type="hidden" value="<?php echo $co_first_name_one?>" name="co_first_name_one">
+    <input type="hidden" value="<?php echo $co_middle_name_one?>" name="co_middle_name_one">
+    <input type="hidden" value="<?php echo $co_last_name_one?>" name="co_last_name_one">
+    <input type="hidden" value="<?php echo $co_contact_no_one?>" name="co_contact_no_one">
+    <input type="hidden" value="<?php echo $co_address_one?>" name="co_address_one">
+    <input type="hidden" value="<?php echo $co_business_address_one?>" name="co_business_address_one">
+    <input type="hidden" value="<?php echo $co_name_of_firm_one?>" name="co_name_of_firm_one">
+    <input type="hidden" value="<?php echo $co_employment_one?>" name="co_employment_one">
+    <input type="hidden" value="<?php echo $co_position_one?>" name="co_position_one">
+    
+    <input type="hidden" value="<?php echo $co_first_name_two?>" name="co_first_name_two">
+    <input type="hidden" value="<?php echo $co_middle_name_two?>" name="co_middle_name_two">
+    <input type="hidden" value="<?php echo $co_last_name_two?>" name="co_last_name_two">
+    <input type="hidden" value="<?php echo $co_contact_no_two?>" name="co_contact_no_two">
+    <input type="hidden" value="<?php echo $co_address_two?>" name="co_address_two">
+    <input type="hidden" value="<?php echo $co_business_address_two?>" name="co_business_address_two">
+    <input type="hidden" value="<?php echo $co_name_of_firm_two?>" name="co_name_of_firm_two">
+    <input type="hidden" value="<?php echo $co_employment_two?>" name="co_employment_two">
+    <input type="hidden" value="<?php echo $co_position_two?>" name="co_position_two">
+</form>
+ <a href="ClientLoan.php?client_id=<?php echo $client_id?>" id="resend"></a>
+</body>
+</html>
---
+++ LoanMonitoringSystem/SummaryOfBookings.php
@@ -0,0 +1,144 @@
+<?php
+
+  include 'notification_fetch.php'; 
+  include 'navigation.php';
+?>
+<?php
+session_start();
+//Checking User Logged or Not
+if(empty($_SESSION['user'])){
+    header('location:index.php');
+}
+
+
+//timeout after 5 sec
+if(isset($_SESSION['user'])) {
+    if((time() - $_SESSION['last_time']) > 1800) {
+      header("location:logout.php");  
+    }
+}
+
+?>
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Summary of Bookings</title>  
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="css/w3.css">
+          <link rel="stylesheet" type="text/css" href="css/table.css">
+          <link rel="stylesheet" type="text/css" href="css/custom.css">
+          <link rel="stylesheet" type="text/css" href="css/modal.css">
+          <link rel="stylesheet" type="text/css" href="css/style.css">
+          <link rel="stylesheet" type="text/css" href="css/search.css">
+          <link rel="stylesheet" type="text/css" href="css/notification.css">
+          <link rel="stylesheet" type="text/css" href="css/bootstrap.min2.css">
+          <link rel="stylesheet" type="text/css" href="css/navigation.css">
+          <link rel="stylesheet" type="text/css" href="css/navigation2.css">
+          <link rel="stylesheet" type="text/css" href="css/dashboard.css">
+          <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.min.css" /> 
+      </head>  
+      <body>  
+        <nav id="myNavbar" class="navbar nav-color" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="dashboard.php">SIGMA</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <?php
+                        echo navigate_it()
+                    ?>
+                    <ul class="nav navbar-nav navbar-right">
+                    <?php
+                      echo navigate_right();
+
+                      ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+           <div class="container pad-1">
+                <h3 align="center">Summary of Bookings</h3>  
+                <br />   
+                     <table id="employee_data" class="table">    
+                          <?php
+                          include "Include/connection.php";
+                             $query ="SELECT *  from client 
+                                      INNER JOIN loan on client.client_id = loan.client_id
+                                      INNER JOIN payment on payment.loan_id = loan.loan_id
+                                      group by loan.loan_id";  
+                             $result = mysqli_query($conn, $query);
+
+                             echo ' <thead>
+                                      <tr class="text-white">
+                                          <th class="my-bg text-white">Account Name</th>
+                                          <th class="my-bg text-white" >Date Booked</th>
+                                          <th class="my-bg text-white">Maturity Date</th>
+                                          <th class="my-bg text-white" >Original Amount</th>
+                                          <th class="my-bg text-white">Net Proceeds</th>
+                                          <th class="my-bg text-white" >Interest (3 Month)</th>
+                                          <th class="my-bg text-white" >Interest (1 Month)</th>
+                                          <th class="my-bg text-white">Service Handling</th>
+                                          <th class="my-bg text-white" >Other Income</th>
+                                      </tr> 
+                                    </thead>';
+
+                             while($row = mysqli_fetch_array($result))
+                              {         
+                                 
+                                $loan_id = $row['loan_id'];
+                                $sql ="SELECT first_name, last_name, middle_name,date_booked, maturity_date, original_amount, 
+                                loan_balance as net_proceeds,
+                                ROUND(original_amount * 0.15 * (COUNT(due_date)/2), 2) as Interest_earned_3,
+                                ROUND(original_amount * 0.05 * (COUNT(due_date)/2), 2) as Interest_earned_1, 
+                                original_amount*0.03 as service_handling_fee, insurance +coalesce((SELECT SUM(other_income) from payment_info WHERE payment.loan_id='$loan_id' && status='Updated'),0) as other_income  from client 
+                                INNER JOIN loan on client.client_id = loan.client_id
+                                INNER JOIN payment on payment.loan_id = loan.loan_id 
+                                WHERE loan.loan_id='$loan_id'";
+                                
+                                $rowSummary = mysqli_fetch_assoc(mysqli_query($conn,$sql));
+                                
+                                $firstName = htmlspecialchars($rowSummary['first_name']);
+                                $middleName = htmlspecialchars($rowSummary['middle_name']);
+                                $lastName = htmlspecialchars($rowSummary['last_name']);
+                                $dateBooked = htmlspecialchars($rowSummary['date_booked']);
+                                $maturityDate = htmlspecialchars($rowSummary['maturity_date']);
+                                $originalAmount = htmlspecialchars($rowSummary['original_amount']);
+                                $netProceeds = htmlspecialchars($rowSummary['net_proceeds']);
+                                $Interest_3 = htmlspecialchars($rowSummary['Interest_earned_3']);
+                                $Interest_1 = htmlspecialchars($rowSummary['Interest_earned_1']);
+                                $serviceHandling = htmlspecialchars($rowSummary['service_handling_fee']);
+                                $otherIncome = htmlspecialchars($rowSummary['other_income']);
+                                 
+                                        echo  '
+                                              <tr>
+                                                  <td> <a href="Profile.php?loan_id='.$row["loan_id"].'">'.$firstName.'  '.$middleName.' '.$lastName.'</a></td>
+                                                  <td>'.$maturityDate.'</td>
+                                                  <td>'.$dateBooked.'</td>
+                                                  <td>'.$originalAmount.'</td>
+                                                  <td>'.$netProceeds.'</td>
+                                                  <td>'.$Interest_3.'</td>
+                                                  <td>'.$Interest_1.'</td>
+                                                  <td>'.$serviceHandling.'</td>
+                                                  <td>'.$otherIncome.'</td>
+                                             </tr>';
+
+                              }  
+                          ?>  
+                     </table>
+          </div>
+            <script src="js/ajax.js"></script> 
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/jquery.dataTables.min.js"></script>  
+            <script src="js/dataTables.bootstrap.min.js"></script>
+            <script>
+                  $(document).ready(function(){
+
+                      $('#employee_data').DataTable();
+
+                  });
+          </script> 
+
+      </body>  
+ </html>  
